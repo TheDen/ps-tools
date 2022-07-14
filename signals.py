f77c7b0ae4eb7a1837ee62041a8d37765735442b
TheDen <deniskhoshaba@gmail.com>
2022-07-15 01:18:29 +1000

    update signals

--- signals.py
+++ signals.py
@@ -21,10 +21,19 @@ def signal_handler(signum, frame):
     print(f"\nReceived signal {signal.Signals(signum).name}")
 
 
+def print_signals(signals):
+    print("List of signals:")
+    for sig in set(signals):
+        print(f"{sig:<2}: {sig.name}")
+    print("")
+
+
 if __name__ == "__main__":
     pid = os.getpid()
     print_info(pid)
+    signals = signal.Signals
+    print_signals(signals)
     while True:
-        catchable_sigs = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
+        catchable_sigs = set(signals) - {signal.SIGKILL, signal.SIGSTOP}
         for sig in catchable_sigs:
             signal.signal(sig, signal_handler)
